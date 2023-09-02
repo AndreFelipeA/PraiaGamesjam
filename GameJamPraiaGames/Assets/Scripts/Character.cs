7d4860b8cd8e9eca8388709a1f5586b51633f764
MickeyNilson <camargonick@hotmail.com>
2023-09-02 17:45:11 -0300

    Change: Ghost Animation Controller & script

--- GameJamPraiaGames/Assets/Scripts/Character.cs
+++ GameJamPraiaGames/Assets/Scripts/Character.cs
@@ -6,25 +6,33 @@ using UnityEngine.Events;
 public class Character : MonoBehaviour
 {
     float horizontalMove = 0f;
-    bool canScare = true;
     int cooldown;
     public Animator animator;
     UnityEvent KeyPress;
     // Start is called before the first frame update
     void Start()
     {
-        if(KeyPress == null) KeyPress = new UnityEvent();
+        //Codigo do Andras
 
-        KeyPress.AddListener(Scare);
-        cooldown = 20; 
+        //if(KeyPress == null) KeyPress = new UnityEvent();
+
+        //KeyPress.AddListener(Scare);
+        //cooldown = 20; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontalMove = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("speed", horizontalMove);
-        if(Input.GetKeyDown("h") && KeyPress != null)
+        cooldown--;
+        if (cooldown < 0)
+            cooldown = 0;
+
+        Scare();
+        movementAnimation();
+
+        //Codigo do Andras
+
+        /*if(Input.GetKeyDown("h") && KeyPress != null)
         {
             KeyPress.Invoke();
         }
@@ -32,15 +40,25 @@ public class Character : MonoBehaviour
             cooldown--;
         }else if(cooldown == 0){
             canScare = true;
-        }
+        }*/
 
     }
     void Scare()
     {
-        if(canScare){ 
-            //placeholder
-        }else{
-            //placeholder
+        if (Input.GetKeyDown("h") && cooldown == 0){
+            animator.SetBool("scaring", true);
+            cooldown = 200;
         }
+        if (cooldown < 19.5)
+            animator.SetBool("scaring", false);
+    }
+
+    void movementAnimation()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal");
+        if (horizontalMove != 0)
+            animator.SetBool("moving", true);
+        else
+            animator.SetBool("moving", false);
     }
 }
