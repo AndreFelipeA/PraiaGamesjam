0ee5ef3d9082c58e759dfbd14db79daffcd7458e
AndreFelipeA <104396880+AndreFelipeA@users.noreply.github.com>
2023-09-02 00:53:29 -0300

    Add Player Sprite Flip

--- GameJamPraiaGames/Assets/Scripts/PlayerMovement.cs
+++ GameJamPraiaGames/Assets/Scripts/PlayerMovement.cs
@@ -14,6 +14,8 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     
     private float smoothInputSpeed;
+
+    private bool facingLeft = false;
     // Update is called once per frame
     void Update()
     {
@@ -32,10 +34,27 @@ public class PlayerMovement : MonoBehaviour
         float moveY = Input.GetAxisRaw("Vertical");
         UnityEngine.Vector2 input = new UnityEngine.Vector2(moveX,moveY);
         moveDir = Vector2.SmoothDamp(moveDir, input, ref smoothInputVelocity, smoothInputSpeed);
+
+        if(moveX > 0 && !facingLeft)
+        {
+            Flip();
+        }
+        if(moveX < 0 && facingLeft)
+        {
+            Flip();
+        }
     }
 
     void Move()
     {
         rb.velocity = new Vector2(moveDir.x * moveSpeed, moveDir.y * moveSpeed);
+    }
+
+    void Flip()
+    {
+        UnityEngine.Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+        facingLeft = !facingLeft;
     }
 }
